48f6f46655e4b4d89bf59e4a4c7e8c550872e6a4
vagrant <vagrant@localhost.localdomain>
2020-07-19 14:55:42 +0000

    asd

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,8 +1,5 @@
 class Genre < ApplicationRecord
-<<<<<<< HEAD
 	has_many :products
 	enum is_enabled: { 有効: true, 無効: false}
-=======
 	has_many :products, dependent: :destroy
->>>>>>> origin/develop
 end
