6aefee6e2290feefa0ccc21d58753c1fe284067e
vagrant <vagrant@localhost.localdomain>
2020-07-19 07:39:33 +0000

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_07_19_043136) do
-=======
-ActiveRecord::Schema.define(version: 2020_07_16_032638) do
->>>>>>> origin/develop
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,18 +46,17 @@ ActiveRecord::Schema.define(version: 2020_07_16_032638) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_phonetic"
     t.string "last_name_phonetic"
     t.string "telephone_number"
     t.string "postcode"
-    t.string "string"
     t.string "address"
     t.boolean "is_deleted"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
