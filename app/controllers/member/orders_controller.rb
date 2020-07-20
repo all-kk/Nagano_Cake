d148530c16146aac4fb25d3b0a5dd1e514223867
vagrant <vagrant@localhost.localdomain>
2020-07-20 09:45:34 +0000

    error

--- app/controllers/member/orders_controller.rb
+++ app/controllers/member/orders_controller.rb
@@ -2,6 +2,7 @@ class Member::OrdersController < ApplicationController
 	def new
 		@order = Order.new
 		@member = current_member
+		@shippings = Shipping.all
 	end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,5 @@
 class Order < ApplicationRecord
-<<<<<<< HEAD
 	has_many :order_details, dependent: :destroy
 	belongs_to :member
-	enum payment_method: { クレジットカード:1, 銀行振り込み:2 }
-=======
->>>>>>> origin/develop
+	enum payment_method: { クレジットカード:0, 銀行振り込み:1 }
 end
