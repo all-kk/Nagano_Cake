493de66a82ea06853e78dcfb49eef312b3aa2e42
vagrant <vagrant@localhost.localdomain>
2020-07-21 12:24:04 +0000

    a

--- app/controllers/member/orders_controller.rb
+++ app/controllers/member/orders_controller.rb
@@ -1,8 +1,12 @@
 class Member::OrdersController < ApplicationController
 
 	def thanks
-		@order = Order.find(params[:id])
+		
 	end
+	def index
+		@orders = current_member.orders
+	end
+
 
 	def new
 		@order = Order.new(order_params)
@@ -16,35 +20,35 @@ class Member::OrdersController < ApplicationController
 		@total = 0
 
 		if    params[:order][:address_number] == "0"
-			  @order.postcode = current_member.postcode
-			  @order.address = current_member.address
-			  @order.name = current_member.last_name
+			@order.postcode = current_member.postcode
+			@order.address = current_member.address
+			@order.name = current_member.last_name
 
 		elsif params[:order][:address_number] == "1"
-			  @order.postcode = Shipping.find(params[:order][:shipping]).postcode
-			  @order.address = Shipping.find(params[:order][:shipping]).address
-			  @order.name = Shipping.find(params[:order][:shipping]).name
+			@order.postcode = Shipping.find(params[:order][:shipping]).postcode
+			@order.address = Shipping.find(params[:order][:shipping]).address
+			@order.name = Shipping.find(params[:order][:shipping]).name
 
 		elsif params[:order][:address_number] ==  "2"
-			  @shipping = Shipping.new(shipping_params)
-			  @shipping.postcode = params[:order][:postcode]
-			  @shipping.address = params[:order][:address]
-			  @shipping.name = params[:order][:name]
-			  @shipping.member_id = current_member.id
+			@shipping = Shipping.new(shipping_params)
+			@shipping.postcode = params[:order][:postcode]
+			@shipping.address = params[:order][:address]
+			@shipping.name = params[:order][:name]
+			@shipping.member_id = current_member.id
 			
 			if  @shipping.save
 				@order.postcode = @shipping.postcode
 				@order.address = @shipping.address
 				@order.name = @shipping.name
 			end
-        end
+		end
 	end
 
 	def create
 		@order = Order.new(order_params)
 		@order.member_id = current_member.id
 		@order.save
-	    redirect_to member_thanks_order_path(current_member.id)
+		redirect_to member_thanks_order_path(current_member.id)
 	end
 
 	
