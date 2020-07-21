9c7ddb566f81741b4884141c0e2ac61f65ee43f9
vagrant <vagrant@localhost.localdomain>
2020-07-21 05:46:05 +0000

    a

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	attachment :image
 	belongs_to :genre
 	has_many :order_details
-	belongs_to :cart_item
+	belongs_to :cart_item, optional: true
 	has_many :order_details, dependent: :destroy
 	enum is_capable: { 販売中: true, 販売停止中: false}
 	validates :price, presence: true
